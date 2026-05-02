3bab5eb54e5c4ad277fc9f194ac0664dfcca4b8e
Jimmy Vega <jimmy.o.vega@gmail.com>
2026-05-01 21:07:06 -0400

    Updated worker to use contact@azurelens.work

--- src/worker.js
+++ src/worker.js
@@ -128,7 +128,7 @@ IP: ${ipAddress}
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        from: 'Azure Lens Contact Form <onboarding@resend.dev>',
+        from: 'Azure Lens Contact Form <contact@azurelens.work>',
         to: [recipientEmail],
         reply_to: `${name} <${email}>`,
         subject: `[Azure Lens] Contact: ${subject}`,
